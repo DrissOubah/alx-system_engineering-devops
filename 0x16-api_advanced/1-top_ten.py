b14502c041acf8f5bdf05c8e2fccd3cc7903c0d1
DrissOubah <drissoubahmane@gmail.com>
2024-08-14 17:59:58 -0700

    fix 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     10 hot posts listed for a given subreddit
     """
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "YourUserAgent"
         }
