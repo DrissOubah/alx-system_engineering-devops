5d786b9c66598b4b7c6e366e0f0a19c6a1e892c5
DrissOubah <drissoubahmane@gmail.com>
2024-08-14 17:44:28 -0700

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+"""Function to get the number of subscribers for a specified Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """Fetch and return the subscriber count for the specified subreddit."""
+    endpoint = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(endpoint, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
